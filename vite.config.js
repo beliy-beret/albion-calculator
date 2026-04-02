04d1a41205b40b042fca72a86553d20d3362c161
Stanislav Skorobogatot <skoriy12990@gmail.com>
2026-04-02 21:26:14 +0300

    add github page

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/albion-calculator/',
+
   server: {
     port: 3000,
     open: true
   },
+  
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
